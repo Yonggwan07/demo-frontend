dd6944cdf1e1e58c5c41d01ae976232b4a4cb423
Park Yonggwan <yk9007@naver.com>
2022-04-28 11:41:30 +0900

    add LoginPage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import { Routes, Route } from '../node_modules/react-router-dom/index';
 import './App.css';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </>
   );
 }
 
