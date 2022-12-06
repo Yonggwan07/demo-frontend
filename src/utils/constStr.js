7ddb0546e54b980c3a6034584ddf598dd5463c3e
Park Yonggwan <pyk@inbus.co.kr>
2022-12-06 12:25:03 +0900

    constStr

---
+++ src/utils/constStr.js
@@ -0,0 +1,12 @@
+export const constStr = {
+  postSearch: (count) => `${count}건이 조회되었습니다.`,
+  errorSearch: '조회에 실패했습니다.',
+  postSave: (count) => `${count}건이 저장되었습니다.`,
+  errorSave: '저장에 실패했습니다.',
+  required: '필수 입력',
+  maxLength: (length) => ({
+    value: length,
+    message: `${length}자 이하 입력`,
+  }),
+  minLength: (length) => ({ value: length, message: `${length}자 이상 입력` }),
+};
