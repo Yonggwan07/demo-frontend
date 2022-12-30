0c85669f645d5dd8d7a13484320acee798f1db9b
Park Yonggwan <pyk@inbus.co.kr>
2022-12-30 11:48:34 +0900

    Datagrid DatePicker 적용

--- src/hooks/useDatagrid.js
+++ src/hooks/useDatagrid.js
@@ -1,17 +1,28 @@
+import { InputBase } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import { useGridApiContext } from '@mui/x-data-grid';
+import { format, parseISO } from 'date-fns';
+import ko from 'date-fns/locale/ko';
 import { useEffect, useState } from 'react';
+import DatePicker from 'react-datepicker';
+
+const StyledInput = styled(InputBase)({
+  '& .MuiInputBase-input': {
+    fontSize: '0.875rem',
+  },
+});
 
 const useDatagrid = (columnInfo, commCodes) => {
   const [rows, setRows] = useState([]);
   const [columns, setColumns] = useState([]);
 
+  // Select
   const SelectInputCell = (props) => {
     const { value, options } = props;
     const searched = options.find((option) => option.comdCode === value);
     return <div>{searched !== undefined ? searched.comdCdnm : ''}</div>;
   };
 
-  // DataGrid Select Edit Cell Renderer
   const SelectEditInputCell = (props) => {
     const { id, value, field, options } = props;
     const apiRef = useGridApiContext();
@@ -37,6 +48,65 @@ const useDatagrid = (columnInfo, commCodes) => {
     );
   };
 
+  // DatePicker
+  const DatePickerEditCell = (props) => {
+    const { id, value, field } = props;
+    const apiRef = useGridApiContext();
+    const minDate =
+      props.minDate && apiRef.current.getCellValue(id, props.minDate);
+    const maxDate =
+      props.maxDate && apiRef.current.getCellValue(id, props.maxDate);
+
+    const handleChange = async (date) => {
+      await apiRef.current.setEditCellValue({
+        id,
+        field,
+        value: format(date, 'yyyyMMdd'),
+      });
+      apiRef.current.stopCellEditMode({ id, field });
+    };
+
+    return (
+      <DatePicker
+        dateFormat="yyyy-MM-dd"
+        selected={dateFormatter(value)}
+        minDate={
+          minDate &&
+          parseISO(
+            minDate.substr(0, 4) +
+              '-' +
+              minDate.substr(4, 2) +
+              '-' +
+              minDate.substr(6, 2),
+          )
+        }
+        maxDate={
+          maxDate &&
+          parseISO(
+            maxDate.substr(0, 4) +
+              '-' +
+              maxDate.substr(4, 2) +
+              '-' +
+              maxDate.substr(6, 2),
+          )
+        }
+        locale={ko}
+        onChange={handleChange}
+        readOnly={props.readOnly}
+        customInput={<StyledInput />}
+      />
+    );
+  };
+
+  const dateFormatter = (date) => {
+    return (
+      date &&
+      parseISO(
+        date.substr(0, 4) + '-' + date.substr(4, 2) + '-' + date.substr(6, 2),
+      )
+    );
+  };
+
   useEffect(() => {
     if (Object.keys(commCodes).length > 0) {
       for (const key in columnInfo) {
@@ -54,6 +124,27 @@ const useDatagrid = (columnInfo, commCodes) => {
               <SelectEditInputCell
                 {...params}
                 options={commCodes[columnInfo[key].field]}
+              />
+            );
+          };
+        } else if (columnInfo[key].compType === 'date') {
+          columnInfo[key].renderCell = (params) => {
+            return (
+              <DatePicker
+                dateFormat="yyyy-MM-dd"
+                selected={dateFormatter(params.value)}
+                locale={ko}
+                readOnly
+                customInput={<StyledInput />}
+              />
+            );
+          };
+          columnInfo[key].renderEditCell = (params) => {
+            return (
+              <DatePickerEditCell
+                {...params}
+                minDate={columnInfo[key].minDate}
+                maxDate={columnInfo[key].maxDate}
               />
             );
           };
