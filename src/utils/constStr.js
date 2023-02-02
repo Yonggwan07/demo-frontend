d105a053be978338be2a66335b976a1336b35952
Park Yonggwan <pyk@inbus.co.kr>
2023-02-02 15:30:41 +0900

    저장, 삭제 메시지 수정

--- src/utils/constStr.js
+++ src/utils/constStr.js
@@ -1,9 +1,9 @@
 export const constStr = {
   postSearch: (count) => `${count}건이 조회되었습니다.`,
   errorSearch: '조회에 실패했습니다.',
-  postSave: (count) => `${count}건이 저장되었습니다.`,
+  postSave: '저장되었습니다.',
   errorSave: '저장에 실패했습니다.',
-  postDelete: (count) => `${count}건이 삭제되었습니다.`,
+  postDelete: '삭제되었습니다.',
   errorDelete: '삭제에 실패했습니다.',
   required: '필수 입력',
   maxLength: (length) => ({
