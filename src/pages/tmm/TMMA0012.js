9679a915aebe7f94299c0f082ba321e5768b70b3
Park Yonggwan <pyk@inbus.co.kr>
2023-02-06 15:37:51 +0900

    변경된 필드명 적용

--- src/pages/tmm/TMMA0012.js
+++ src/pages/tmm/TMMA0012.js
@@ -8,45 +8,35 @@ import useCombo from '../../hooks/useCombo';
 import useDatagrid from '../../hooks/useDatagrid';
 import { commonMenuPropType } from '../../utils/commonMenuPropType';
 
-const codeOptions = [
-  { commCode: 'SYST_CODE', usexYsno: '1' },
-  { commCode: 'CDGB_CODE', usexYsno: '1' },
-];
+const codeOptions = ['SYST_CODE', 'CDGB_CODE'];
 const mColumnInfo = [
-  { field: 'COMM_CODE', headerName: '공통코드', width: 150 },
-  { field: 'COMM_CDNM', headerName: '공통코드명', flex: 1 },
-  { field: 'SYST_CODE', headerName: '시스템코드' },
-  { field: 'CDGB_CODE', headerName: '코드구분코드' },
-  { field: 'COCD_LNTH', headerName: '세부코드길이' },
-  { field: 'RE1F_DESC', headerName: '보조1필드설명' },
-  { field: 'RE2F_DESC', headerName: '보조2필드설명' },
-  { field: 'RE3F_DESC', headerName: '보조3필드설명' },
-  { field: 'RE4F_DESC', headerName: '보조4필드설명' },
-  { field: 'RE5F_DESC', headerName: '보조5필드설명' },
-  { field: 'RE6F_DESC', headerName: '보조6필드설명' },
-  { field: 'REMK_100X', headerName: '비고100' },
+  { field: 'commonCode', headerName: '공통코드', width: 150 },
+  { field: 'commonCodeName', headerName: '공통코드명', flex: 1 },
+  { field: 'systemCode', headerName: '시스템코드' },
+  { field: 'divisionCode', headerName: '코드구분코드' },
+  { field: 'commonCodeLength', headerName: '세부코드길이' },
+  { field: 'note', headerName: '비고' },
 ];
 
 const dColumnInfo = [
-  { field: 'COMM_CODE', headerName: '공통코드' },
-  { field: 'COMD_CODE', headerName: '세부코드', width: 150 },
-  { field: 'COMD_CDNM', headerName: '세부코드명', width: 200 },
-  { field: 'COMD_ENNM', headerName: '영문명', width: 200 },
-  { field: 'CRTE_DATE', headerName: '생성일', width: 200 },
-  { field: 'WAST_DATE', headerName: '폐기일', width: 200 },
-  { field: 'REF1_FILD', headerName: '보조1필드' },
-  { field: 'REF2_FILD', headerName: '보조2필드' },
-  { field: 'REF3_FILD', headerName: '보조3필드' },
-  { field: 'REF4_FILD', headerName: '보조4필드' },
-  { field: 'REF5_FILD', headerName: '보조5필드' },
-  { field: 'REF6_FILD', headerName: '보조6필드' },
-  { field: 'REF7_FILD', headerName: '보조7필드' },
-  { field: 'REF8_FILD', headerName: '보조8필드' },
-  { field: 'REF9_FILD', headerName: '보조9필드' },
-  { field: 'RE10_FILD', headerName: '보조10필드' },
-  { field: 'SORT_ORDR', headerName: '정렬순서' },
-  { field: 'REMK_100X', headerName: '비고100' },
-  { field: 'OERP_CODE', headerName: '(구)ERP코드' },
+  { field: 'commonCode', headerName: '공통코드' },
+  { field: 'commonDetailCode', headerName: '세부코드', width: 150 },
+  { field: 'commonDetailCodeName', headerName: '세부코드명', width: 200 },
+  { field: 'commonDetailCodeEnglishName', headerName: '영문명', width: 200 },
+  { field: 'createdDate', headerName: '생성일', width: 200, compType: 'date' },
+  { field: 'wastedDate', headerName: '폐기일', width: 200, compType: 'date' },
+  { field: 'refField1', headerName: '보조필드1' },
+  { field: 'refField2', headerName: '보조필드2' },
+  { field: 'refField3', headerName: '보조필드3' },
+  { field: 'refField4', headerName: '보조필드4' },
+  { field: 'refField5', headerName: '보조필드5' },
+  { field: 'refField6', headerName: '보조필드6' },
+  { field: 'refField7', headerName: '보조필드7' },
+  { field: 'refField8', headerName: '보조필드8' },
+  { field: 'refField9', headerName: '보조필드9' },
+  { field: 'refField10', headerName: '보조필드10' },
+  { field: 'sortOrder', headerName: '정렬순서' },
+  { field: 'note', headerName: '비고100' },
 ];
 
 const TMMA0012 = ({ menuInfo, search }) => {
@@ -66,7 +56,7 @@ const TMMA0012 = ({ menuInfo, search }) => {
   /* 조회 버튼 클릭 */
   const handleSearch = useCallback(
     (data) => {
-      search(menuInfo.id, 'TmCodexh', data)
+      search(menuInfo.id, data, 'header')
         .then((res) => {
           setMasterRows(res);
         })
@@ -81,8 +71,8 @@ const TMMA0012 = ({ menuInfo, search }) => {
     (e) => {
       search(
         menuInfo.id,
-        'TmCodexd',
-        { COMM_CODE: e.row.COMM_CODE },
+        { commonCode: e.row.commonCode },
+        'detail',
         false,
       ).then((res) => {
         setDetailRows(res);
@@ -127,16 +117,10 @@ const TMMA0012 = ({ menuInfo, search }) => {
             initialState={{
               columns: {
                 columnVisibilityModel: {
-                  SYST_CODE: false,
-                  CDGB_CODE: false,
-                  COCD_LNTH: false,
-                  RE1F_DESC: false,
-                  RE2F_DESC: false,
-                  RE3F_DESC: false,
-                  RE4F_DESC: false,
-                  RE5F_DESC: false,
-                  RE6F_DESC: false,
-                  REMK_100X: false,
+                  systemCode: false,
+                  divisionCode: false,
+                  commonCodeLength: false,
+                  note: false,
                 },
               },
             }}
@@ -148,17 +132,17 @@ const TMMA0012 = ({ menuInfo, search }) => {
           initialState={{
             columns: {
               columnVisibilityModel: {
-                COMM_CODE: false,
-                REF1_FILD: false,
-                REF2_FILD: false,
-                REF3_FILD: false,
-                REF4_FILD: false,
-                REF5_FILD: false,
-                REF6_FILD: false,
-                REF7_FILD: false,
-                REF8_FILD: false,
-                REF9_FILD: false,
-                RE10_FILD: false,
+                commonCode: false,
+                refField1: false,
+                refField2: false,
+                refField3: false,
+                refField4: false,
+                refField5: false,
+                refField6: false,
+                refField7: false,
+                refField8: false,
+                refField9: false,
+                refField10: false,
               },
             },
           }}
